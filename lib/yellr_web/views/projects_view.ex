d93be72c7c50a71cd63c32b7da347e27a532fddc
Trey Evans <tevans@justplainawful.lan>
2020-04-20 15:36:55 -0400

    Sort branches by name in project show.

--- lib/yellr_web/views/projects_view.ex
+++ lib/yellr_web/views/projects_view.ex
@@ -1,3 +1,8 @@
 defmodule YellrWeb.ProjectsView do
   use YellrWeb, :view
+
+  @spec sort_branches_for_display([Yellr.Data.Branch]) :: [Yellr.Data.Branch]
+  def sort_branches_for_display(branches) do
+    Enum.sort_by(branches, fn(b) -> b.name end)
+  end
 end
